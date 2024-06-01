a7ea492d7a784ed6cf1b5f6440002e2d93dcb514
rsv6 <rafaelsvbr@gmail.com>
2024-06-01 18:13:55 -0300

    feat: change environment to .env

--- src/createClient.ts
+++ src/createClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-"https://uynscogmlqinalfmodoh.supabase.co",     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5bnNjb2dtbHFpbmFsZm1vZG9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcyMDY3ODIsImV4cCI6MjAzMjc4Mjc4Mn0.kq8juWV5KnYbyfo1yKyAdurSMlc-TzYhzyiRzk1FRr0"
+    import.meta.env.VITE_SUPABASE_URL,  
+    import.meta.env.VITE_SUPABASE_KEY
 );
